74d5b4dbfa6a2c05d14520c9bb68b69f26cd5cac
Nick Fogal <nfogal@gmail.com>
2019-06-27 00:36:20 -0400

    Initial Commit

---
+++ NetworkRequests/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NetworkRequests
+//
+//  Created by Nick Fogal on 6/27/19.
+//  Copyright © 2019 Nick Fogal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NetworkRequests/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NetworkRequests
+//
+//  Created by Nick Fogal on 6/27/19.
+//  Copyright © 2019 Nick Fogal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
